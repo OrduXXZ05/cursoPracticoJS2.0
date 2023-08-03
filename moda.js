9b152880ee1101c55c0b62741420ea68bff733ba
Felipe Orduz <orduzfelipe05@gmail.com>
2023-08-02 20:41:47 -0500

    Encapsule el codigo en una funcion para poder darle uso mas facilmente

--- moda.js
+++ moda.js
@@ -14,23 +14,28 @@ const lista1 = [
     2,
 ];
 
+function calcularModa(lista) {
 
-const lista1Count = {};
+    const listaCount = {};
 
-lista1.map(
-    function (elemento) {
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] += 1;
-        } else{
-            lista1Count[elemento] = 1;
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            } else{
+                listaCount[elemento] = 1;
+            }
         }
-    }
 )
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function (elementoA, elementoB) {
+    const listaArray = Object.entries(listaCount).sort(
+        function (elementoA, elementoB) {
         return elementoA[1] - elementoB[1];
     }
 )
 
-const moda = lista1Array[lista1Array.length - 1];
+    const moda = listaArray[listaArray.length - 1];
+    return moda;
+
+}
+
